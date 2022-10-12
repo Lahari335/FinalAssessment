e02b9b535f56478ba03af8e080285ed8ea035e4a
LAHARI POOJARY <88604091+Lahari335@users.noreply.github.com>
2022-10-12 11:18:42 +0530

    Update Abstract.java

--- src/test/java/Basicoops/Abstract.java
+++ src/test/java/Basicoops/Abstract.java
@@ -1,24 +1,22 @@
 package Basicoops;
-abstract class Abc
+abstract class Vehicle1
 {
 	abstract void m1();
 	void m2()
 	{
-		System.out.println("m2 code");
+		System.out.println("Most prefered mode of transportation");
 	}
 }
-public class Abstract extends Abc {
+public class Abstract extends Vehicle1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Abstract obj=new Abstract();
+		obj.m2();
 		obj.m1();
 	}
 	void m1()
 	{
-		System.out.println("m1 code");
+		System.out.println("bus");
 	}
 }
-
-
-
