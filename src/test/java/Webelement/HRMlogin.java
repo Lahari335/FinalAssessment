048465f72d5a1f0926434e97542e13a6e11960db
LAHARI POOJARY <88604091+Lahari335@users.noreply.github.com>
2022-10-12 14:00:36 +0530

    Update HRMlogin.java

--- src/test/java/Webelement/HRMlogin.java
+++ src/test/java/Webelement/HRMlogin.java
@@ -10,12 +10,12 @@ public class HRMlogin {
 		// TODO Auto-generated method stub
 		 System.setProperty("webdriver.chrome.driver", "C:\\lahari\\chromedriver.exe");
 	        WebDriver driver = new ChromeDriver();
-	        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
-	        driver.manage().window().maximize();
+	        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"); //navigates to the site
+	        driver.manage().window().maximize(); //maximizes the window
 	        Thread.sleep(1000);
-	        driver.findElement(By.name("username")).sendKeys("Admin");
-	        driver.findElement(By.xpath("//input[@name='password']")).sendKeys("admin123");
-	        driver.findElement(By.xpath("//button[@type='submit']")).click();
+	        driver.findElement(By.name("username")).sendKeys("Admin"); //username field
+	        driver.findElement(By.xpath("//input[@name='password']")).sendKeys("admin123"); //password field
+	        driver.findElement(By.xpath("//button[@type='submit']")).click(); //navigates to submit button
 	 
 
 	}
