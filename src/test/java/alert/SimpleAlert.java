e6605c82322b8fba1d527f8068e032a45d4e5e9a
LAHARI POOJARY <88604091+Lahari335@users.noreply.github.com>
2022-10-12 10:23:43 +0530

    Update SimpleAlert.java

--- src/test/java/alert/SimpleAlert.java
+++ src/test/java/alert/SimpleAlert.java
@@ -15,22 +15,14 @@ public class SimpleAlert {
 		// TODO Auto-generated method stub
 		System.setProperty("webdriver.chrome.driver", "C:\\lahari\\chromedriver.exe");
 		  WebDriver driver = new ChromeDriver();
-		  driver.manage().timeouts().implicitlyWait(1000, TimeUnit.SECONDS);
-		  driver.get("https://netbanking.hdfcbank.com/netbanking/");
-		  driver.switchTo().frame("login_page");
-		  driver.findElement(By.linkText("CONTINUE")).click();
-		  Alert HdfcAlert=driver.switchTo().alert();
-		  String actualResult = HdfcAlert.getText();
-		  HdfcAlert.accept();
-		  driver.switchTo().defaultContent();
-		  System.out.println(actualResult);
-		  String expectedResult="Customer ID cannot be left blank.";
-		  Assert.assertEquals(actualResult,expectedResult);
-		  Assert.assertEquals(actualResult,expectedResult,"actual result and expected results are not matching");
-		 SoftAssert softAssert = new SoftAssert();
-		 softAssert.assertEquals(actualResult, expectedResult);
-		 System.out.println("After assertion");
-		
+		  driver.manage().timeouts().implicitlyWait(3000, TimeUnit.SECONDS);
+		driver.get("https://netbanking.hdfcbank.com/netbanking/");//navigate to the page
+		driver.manage().window().maximize();//maximize the window
+		driver.switchTo().frame("login_page");//Switch to the login frame
+		driver.findElement(By.linkText("CONTINUE")).click();//click on the continue button
+		Thread.sleep(5000);
+		Alert hdfc =driver.switchTo().alert();//instantiate alert class
+		hdfc.accept();//to click on OK in alert box
 
 	}
 
