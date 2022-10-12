5001ec4365db07b57e5af1cab75f3a84c0307735
LAHARI POOJARY <88604091+Lahari335@users.noreply.github.com>
2022-10-12 11:29:59 +0530

    Update Collections.java

--- src/test/java/Basicoops/Collections.java
+++ src/test/java/Basicoops/Collections.java
@@ -1,22 +1,32 @@
 package Basicoops;
-
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Iterator;
-
 public class Collections {
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		ArrayList<String> list=new ArrayList<String>();
-		list.add("sun");  
-		list.add("moon");  
-		list.add("stars");  
-		list.add("solar system");  
-		//Traversing list through Iterator  
-		Iterator<String> itr=list.iterator();  
-		while(itr.hasNext()){  
-		System.out.println(itr.next()); 
-
+		Collection values=new ArrayList();
+		System.out.println("List some types of Insurance and its year");
+		values.add("house Insurance");
+		values.add(2003);
+		values.add("car Insurance");
+		values.add(2004);
+		values.add("gold Insurance");
+		values.add(2005);
+		Iterator i= values.iterator();
+		while(i.hasNext())//returns true if there is another line in input
+		{
+			System.out.println(i.next());
+	
+		}
+		System.out.println("Remove the data of car Insurance");
+		values.remove("car Insurance");
+		values.remove(2004);
+		Iterator j= values.iterator();//an object that can be used to loop through collection
+		
+		while(j.hasNext())//returns true if there is another line in input
+		{
+			System.out.println(j.next());
 	}
 
 }
